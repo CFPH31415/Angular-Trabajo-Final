9a08d071e94708f201efb361aa507efc0fcaac72
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 12:05:21 -0500

    Update usuario.service.ts

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -5,15 +5,20 @@ import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signO
   providedIn: 'root'
 })
 export class UsuarioService {
+// El servicio Auth es inyectado en el constructor
 
   constructor(private auth:Auth) { }
+  // Registra un nuevo usuario con un correo electrónico y una contraseña
+
   registerUser({email, password}:any){
     return createUserWithEmailAndPassword(this.auth, email, password);
   }
+// Inicia sesión con un usuario existente y su correo electrónico y contraseña
 
   login({email, password}:any){
     return signInWithEmailAndPassword(this.auth, email, password)
   }
+// Cierra la sesión del usuario actualmente iniciado
 
   logout(){
     return signOut(this.auth);
