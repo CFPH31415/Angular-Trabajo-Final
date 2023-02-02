d6a40a40dc5053a3fd16ca6aa92b6fd1b51088e8
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 11:56:03 -0500

    Update entrar.component.ts

--- src/app/pages/entrar/entrar.component.ts
+++ src/app/pages/entrar/entrar.component.ts
@@ -8,31 +8,46 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   templateUrl: './entrar.component.html',
   styleUrls: ['./entrar.component.css']
 })
+// Inicialización de la clase EntrarComponent
+
 export class EntrarComponent implements OnInit{
+// Definición del formulario formRegistroUser con los campos email y password
 
   formRegistroUser!:FormGroup;
+// Inyección de dependencias del servicio de usuario y el router
 
   constructor(private usuarioService:UsuarioService, private router:Router){
+// Inicialización del formulario formRegistroUser con sus controles email y password
 
     this.formRegistroUser = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
     })
   }
+// Método que se ejecuta al inicializar el componente
 
   ngOnInit(): void {
   }
+// Método que se ejecuta al hacer submit en el formulario formRegistroUser
 
   onSubmit(){
+    // Llama al método registerUser del servicio usuarioService pasándole los valores del formulario
+
     this.usuarioService.registerUser(this.formRegistroUser.value)
+    // Si se registra correctamente, se navega a la ruta /login
+
     .then(response => {
       console.log(response)
       this.router.navigate(['/login']);
     })
+    // Si ocurre un error, se muestra en la consola
+
     .catch(error => console.log(error))
   }
+// Método que navega a la ruta /login
 
   iniciarSesion(){
     this.router.navigate(['/login']);
   }
 }
+
