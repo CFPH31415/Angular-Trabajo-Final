7a1081634d506d0d32f77c21062093b07522dcef
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 11:54:44 -0500

    Update products-interface.ts

--- src/app/interfaces/products-interface.ts
+++ src/app/interfaces/products-interface.ts
@@ -1,4 +1,6 @@
+// Esta es una interface de Producto, que define los atributos de un objeto producto.
 export interface Producto{
+    // Incluye un ID de tipo string, un nombre de producto de tipo string, un costo de tipo número y una foto de tipo string.
     id: string;
     producto: string;
     costo: number;
