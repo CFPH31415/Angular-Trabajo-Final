0d04546100f6d216fd6ce6fd636f87459a1fbdcd
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 12:04:16 -0500

    Update navbar.component.ts

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -7,16 +7,25 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+// Este componente representa la barra de navegación en la aplicación
+
 export class NavbarComponent {
+  // Inyectar los servicios UsuarioService y Router en el componente
 
   constructor(private usuarioservice:UsuarioService, private router:Router){ }
 
+  // Realizar el cierre de sesión del usuario y navegar a la página de inicio de sesión
 
   logout(){
+        // Llamar al método logout del servicio UsuarioService
+
     this.usuarioservice.logout()
     .then(() => {
+            // Navegar a la página de inicio de sesión después de cerrar la sesión con éxito
+
       this.router.navigate(['/login']);
     })
+     // Imprimir un mensaje de error en caso de que ocurra un error durante el cierre de sesión
     .catch(error => console.log(error));
   }
 }
