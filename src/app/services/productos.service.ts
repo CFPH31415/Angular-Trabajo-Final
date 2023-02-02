b0d99358ea0adf58f84a0708f92bb59f0c4a7a1a
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 12:08:32 -0500

    Update productos.service.ts

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,19 +8,40 @@ import { addDoc, collection, deleteDoc, doc } from 'firebase/firestore';
 @Injectable({
   providedIn: 'root'
 })
+// Este código es un servicio de Angular que se encarga de manejar la lógica de los productos.
+
 export class ProductosService {
+  // Se inyecta el servicio de Firestore
 
   constructor( private firestore:Firestore) { }
+    // Método que agrega un producto a la base de datos
+
   addProduct(producto:Producto){
+    // Se obtiene una referencia a la colección de productos en Firestore
+
     const ProductRef = collection(this.firestore, 'producto');
+        // Se llama a la función addDoc para agregar el producto a la colección
+
     return addDoc(ProductRef, producto);
   }
+    // Método que obtiene todos los productos de la base de datos
+
   getProduct():Observable<Producto[]>{
+        // Se obtiene una referencia a la colección de productos en Firestore
+
     const ProductRef = collection(this.firestore, 'producto');
+        // Se llama a la función collectionData para obtener los datos de la colección
+
     return collectionData(ProductRef, {idField:'id'}) as Observable<Producto[]>
   }
+    // Método que elimina un producto de la base de datos
+
   deleteProduct(producto:Producto){
+        // Se obtiene una referencia al documento específico de Firestore
+
     const ProductRef = doc(this.firestore, `producto/${producto.id}`)
+        // Se llama a la función deleteDoc para eliminar el documento
+
     return deleteDoc(ProductRef);
   }
 }
