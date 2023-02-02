7c50a73bd199dabb2c3b0171cfab22cd83b39b37
Carlos Portillo <121558966+CFPH31415@users.noreply.github.com>
2023-02-02 12:03:47 -0500

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,33 +9,49 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+// Este código es un componente de inicio de sesión en Angular
+
 export class LoginComponent implements OnInit{
+  // Se inicializa un FormGroup para el formulario de inicio de sesión
 
   formLogin!:FormGroup;
+  // Se inyectan los servicios necesarios para el funcionamiento del componente
 
   constructor(private usuarioService:UsuarioService, 
     private router:Router){
+          // Se crea el FormGroup con dos campos: email y password
+
     this.formLogin = new FormGroup({
       email: new FormControl(),
       password: new FormControl(),
     })
    }
+  // Método que se ejecuta al inicializar el componente
 
   ngOnInit(): void {
   }
+  // Método que se ejecuta al hacer submit en el formulario
 
   onSubmit(){
+     // Se hace una llamada al método login del servicio usuarioService
+
     this.usuarioService.login(this.formLogin.value)
     .then(
       response => {
+      // En caso de éxito, se redirige al usuario a la página de inicio
+
         this.router.navigate(['/home'])
         console.log(response);
       }
     )
+            // En caso de error, se muestra en consola
+
     .catch(error => console.log(error));
   }
+  // Método que redirige al usuario a la página de registro
 
   registrarse(){
     this.router.navigate(['/entrar']);
   }
 }
+
